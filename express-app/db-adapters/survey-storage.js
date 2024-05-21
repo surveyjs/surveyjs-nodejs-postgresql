f2cf61288912d99c508d640ade0f5546eb1ec10f
RomanTsukanov <sergeich16@gmail.com>
2024-05-21 11:33:02 +0400

    Update code alignment

--- express-app/db-adapters/survey-storage.js
+++ express-app/db-adapters/survey-storage.js
@@ -1,50 +1,50 @@
 let currentId = 1;
 
 function SurveyStorage (dbQueryAdapter) {
-    function addSurvey (name, callback) {
-      const newObj = {
-        name: name || ("New Survey" + " " + currentId++),
-        json: "{}"
-      };
-      dbQueryAdapter.create("surveys", newObj, id => { 
-        newObj.id = id;
-        callback(newObj);
-      });
-    }
-  
-    function postResults (postId, json, callback) {
-      const newObj = {
-        postid: postId,
-        json: json
-      };
-      dbQueryAdapter.create("results", newObj, id => { 
-        newObj.id = id;
-        callback(newObj);
-      });
-    }
-  
-    return {
-      addSurvey: addSurvey,
-      getSurvey: (surveyId, callback) => {
-        dbQueryAdapter.retrieve("surveys", [{ name: "id", op: "=", value: "'" + surveyId + "'" }], (results) => { callback(results[0]); });
-      },
-      storeSurvey: (id, _, json, callback) => {
-        dbQueryAdapter.update("surveys", { id: id, json: json }, (results) => { callback(results); });
-      },
-      getSurveys: (callback) => {
-        dbQueryAdapter.retrieve("surveys", [], (results) => { callback(results); });
-      },
-      deleteSurvey: (surveyId, callback) => {
-        dbQueryAdapter.delete("surveys", surveyId, (results) => { callback(results); });
-      },
-      postResults: postResults,
-      getResults: (postId, callback) => {
-        dbQueryAdapter.retrieve("results", [{ name: "postid", op: "=", value: "'" + postId + "'" }], (results) => { callback({ id: postId, data: results.map(r => r.json)}); });
-      },
-      changeName: (id, name, callback) => {
-        dbQueryAdapter.update("surveys", { id: id, name: name }, (results) => { callback(results); });
-      }
+  function addSurvey (name, callback) {
+    const newObj = {
+      name: name || ("New Survey" + " " + currentId++),
+      json: "{}"
     };
+    dbQueryAdapter.create("surveys", newObj, id => { 
+      newObj.id = id;
+      callback(newObj);
+    });
   }
 
-  module.exports = SurveyStorage;
+  function postResults (postId, json, callback) {
+    const newObj = {
+      postid: postId,
+      json: json
+    };
+    dbQueryAdapter.create("results", newObj, id => { 
+      newObj.id = id;
+      callback(newObj);
+    });
+  }
+
+  return {
+    addSurvey: addSurvey,
+    getSurvey: (surveyId, callback) => {
+      dbQueryAdapter.retrieve("surveys", [{ name: "id", op: "=", value: "'" + surveyId + "'" }], (results) => { callback(results[0]); });
+    },
+    storeSurvey: (id, _, json, callback) => {
+      dbQueryAdapter.update("surveys", { id: id, json: json }, (results) => { callback(results); });
+    },
+    getSurveys: (callback) => {
+      dbQueryAdapter.retrieve("surveys", [], (results) => { callback(results); });
+    },
+    deleteSurvey: (surveyId, callback) => {
+      dbQueryAdapter.delete("surveys", surveyId, (results) => { callback(results); });
+    },
+    postResults: postResults,
+    getResults: (postId, callback) => {
+      dbQueryAdapter.retrieve("results", [{ name: "postid", op: "=", value: "'" + postId + "'" }], (results) => { callback({ id: postId, data: results.map(r => r.json)}); });
+    },
+    changeName: (id, name, callback) => {
+      dbQueryAdapter.update("surveys", { id: id, name: name }, (results) => { callback(results); });
+    }
+  };
+}
+
+module.exports = SurveyStorage;
